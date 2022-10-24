8097a48648588653a11cfd07e1774c225ef10944
brunombs <99062708+brunombs@users.noreply.github.com>
2022-10-23 22:30:23 -0300

    Add files via upload

---
+++ 21- monologo.py
@@ -0,0 +1,16 @@
+from time import sleep
+print('\033[31m[-#- PYTHON -#-]\033[m \033[32mO que te levou a estudar Python?\033[m\n')
+sleep(3)
+print('\033[31m[-#- PYTHON -#-\033[m] No início de 2022 comecei a questionar o porque de eu estar cursando Direito\n'
+      'e percebi que não era feliz, quando comecei a pensar em alternativas tudo que me vinha a mente tinha relação\n'
+      'com a tecnologia, então pesquisando cada vez mais percebi que era o que queria para a minha vida, comecei\n'
+      'a me dedicar, estudei sobre lógica de programação e em seguida escolhi o Python para começar por conta do\n'
+      'vasto conteúdo disponível na internet além de recomendações. Hoje posso dizer que sou muito mais feliz do que\n'
+      'quando o meu foco era nos estudos de Direito.\n')
+sleep(3)
+print('\033[31m[-#- O FUTURO -#-]\033[m \033[32mO que você espera do futuro?\033[m\n')
+sleep(3)
+print('\033[31m-#- O FUTURO -#-\033[m] Espero que como resultado dos meus esforços, dedicação e estudos eu consiga\n'
+      'encontrar um bom emprego na área, poder contribuir de forma ativa para a sociedade através dos meus conhecimentos\n'
+      'e sei que é totalmente possível tendo em vista que a tecnologia é o futuro!\n'
+      'Continuarei me dedicando e estudando até alcançar os meus objetivos! :)')
