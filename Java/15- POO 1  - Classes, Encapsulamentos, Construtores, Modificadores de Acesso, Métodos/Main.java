c02aed319544eaf88995272f0513b40f77bcf995
brunombs <99062708+brunombs@users.noreply.github.com>
2023-06-09 02:29:43 +0100

    Add files via upload

---
+++ Java/15- POO 1  - Classes, Encapsulamentos, Construtores, Modificadores de Acesso, Métodos/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        Cao cachorro = new Cao();
+        cachorro.nome = "Bobby";
+        cachorro.Anda();
+
+        Cao dog = new Cao("Lulu", 12);
+        dog.DadosCao();
+    }
+}
