be710d6a1086da3a2dd03097c0bfba8d64d85a7c
brunombs <99062708+brunombs@users.noreply.github.com>
2023-07-09 08:25:28 +0100

    Add files via upload

---
+++ Java/24- Polimorfismo/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Quadrado quadrado1 = new Quadrado();
+        quadrado1.desenhar();
+
+        Circulo circulo1 = new Circulo();
+        circulo1.desenhar();
+    }
+}
