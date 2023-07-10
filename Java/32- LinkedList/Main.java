5210a9160f41653523ef179dcf67ff3c63a18229
brunombs <99062708+brunombs@users.noreply.github.com>
2023-07-10 09:47:49 +0100

    Add files via upload

---
+++ Java/32- LinkedList/Main.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> animais = new LinkedList<>();
+
+        animais.add("Cachorro");
+        animais.add("Cavalo");
+        animais.add("Vaca");
+
+        System.out.println("LinkedList: " + animais);
+
+        animais.add(1, "Gato");
+
+        System.out.println("LinkedList: " + animais);
+
+        animais.add(3, "Hamster");
+
+        System.out.println("LinkedList: " + animais);
+    }
+}
